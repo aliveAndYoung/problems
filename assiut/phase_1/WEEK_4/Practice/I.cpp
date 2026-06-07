f4085c8dc446faba24be36cf6326e8323a53cf5d
Youssef <140279042+aliveAndYoung@users.noreply.github.com>
2026-06-07 11:22:45 +0300

    26

--- assiut/phase_1/WEEK_4/Practice/I.cpp
+++ assiut/phase_1/WEEK_4/Practice/I.cpp
@@ -11,7 +11,7 @@ signed main()
     cin.tie(nullptr);
     int n;
     cin >> n;
-    vector<int> _v(n);
+    vector<int> _v(n), _a, _b;
     set<int> _s;
     for (int i = 0; i < n; i++)
         cin >> _v[i], _s.insert(_v[i]);
@@ -23,32 +23,31 @@ signed main()
         cout << 0;
         return 0;
     }
-    // set<int> ima, imi;
-    // for (int i = 0; i < n; i++)
-    // {
-    //     if (_v[i] == maxx)
-    //         ima.insert(i);
-    //     if (_v[i] == minn)
-    //         imi.insert(i);
-    // }
-    int l = 0, r = n - 1, mid, ans = n - 1, a, b, c;
-    while (l <= r)
+    // cout << minn << maxx;
+    for (int i = 0; i < n; i++)
     {
-        mid = l + (r - l) / 2;
-        bool steer = false ;
-        for (int i = 0; i < n; i++)
-        {
-            if (_v[i] == maxx)
-            {
-                a = i + mid;
-                b = i - mid;
-                if (a < n && _v[a] == minn)
-                    steer = true , ans = min(ans, mid);
-                if (b > 0 && _v[b] == minn)
-                    steer = true , ans = min(ans, mid);
-            }
-        }
-        if( steer )
+        if (_v[i] == maxx)
+            _a.push_back(i);
+        if (_v[i] == minn)
+            _b.push_back(i);
     }
+    int ans = n;
+
+    for (int i = 0; i < n; i++)
+    {
+        int a = lower_bound(_a.begin(), _a.end(), i) - _a.begin();
+        if (a == _a.size())
+            a--;
+        int b = lower_bound(_b.begin(), _b.end(), i) - _b.begin();
+        if (b == _b.size())
+            b--;
+        if (a - 1 >= 0 && abs(_a[a - 1] - i) < abs(_a[a] - i))
+            a--;
+        if (b - 1 >= 0 && abs(_b[b - 1] - i) < abs(_b[b] - i))
+            b--;
+        ans = min(ans, abs(_a[a] - _b[b]));
+    }
+    cout << ans;
+
     return 0;
 }
