e6cef74796128fb80402fd24d678e3d859348dda
Youssef <140279042+aliveAndYoung@users.noreply.github.com>
2026-06-28 13:05:10 +0300

    cleanSlate

---
+++ CodeForces/102388B.cpp
@@ -0,0 +1,21 @@
+// by LIFECOULDBEDREAM
+// 2026/06/24 04:17
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define int ll
+signed main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int x1, y1, x2, y2;
+        cin >> x1 >> y1 >> x2 >> y2;
+    }
+
+    return 0;
+}
